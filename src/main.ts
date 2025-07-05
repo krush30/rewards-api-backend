330b21dec56a07e63e69418524a7125c62f12020
krushna <ingalekrushna3003@gmail.com>
2025-07-06 02:08:18 +0530

    Swagger added

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,26 @@
 // src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Rewards API')
+    .setDescription('Backend API for user rewards and redemptions')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document); // <-- Swagger UI path
+
   await app.listen(process.env.PORT || 3000);
   console.log(
-    `🚀 App is running on: http://localhost:${process.env.PORT || 3000}`,
+    `🚀 Server running at http://localhost:${process.env.PORT || 3000}`,
+  );
+  console.log(
+    `📄 Swagger docs at http://localhost:${process.env.PORT || 3000}/api-docs`,
   );
 }
 bootstrap();
